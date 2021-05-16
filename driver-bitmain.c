7b6e6cb8a3c20de1018b9ec1dc98ee7edb8d3276
CrashOverride <cr4shov3rrid3@mynetec.com>
2021-05-16 16:29:53 +0200

    Update driver-bitmain.c
    mynetec copyright added

--- driver-bitmain.c
+++ driver-bitmain.c
@@ -1,6 +1,7 @@
 /*
  * Copyright 2012-2013 Lingchao Xu <lingchao.xu@bitmaintech.com>
  * Copyright 2014-2015 Andrew Smith
+ * Copyright 2017-2021 Eric Günther <cr4shov3rrid3@mynetec.com>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
